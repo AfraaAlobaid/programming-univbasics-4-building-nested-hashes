fa948fbe355cc75fba31ab96fbca3999acdabeb1
AfraaAlobaid <afraa.obaid1@gmail.com>
2022-01-15 08:48:10 +1030

    Practiced updating nested hashes

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,9 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-
+  {
+    railroads: {}
+  }
 end
 
 def monopoly_with_second_tier
@@ -9,7 +11,13 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-
+  result = base_hash
+  result[:railroads] = {
+    pieces: 4,
+    rent_in_dollars: {},
+    names: {}
+  }
+  result
 end
 
 def monopoly_with_third_tier
@@ -17,7 +25,21 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  result = monopoly_with_second_tier
+  result[:railroads][:rent_in_dollars] = {
+    one_piece_owned: 25,
+    two_pieces_owned: 50,
+    three_pieces_owned: 100,
+    four_pieces_owned: 200
+  }
 
+  result[:railroads][:names] = {
+    reading_railroad: {},
+    pennsylvania_railroad: {},
+    b_and_o_railroad: {},
+    shortline_railroad: {}
+  }
+  result
 end
 
 def monopoly_with_fourth_tier
@@ -25,5 +47,11 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  result = monopoly_with_third_tier
+  result[:railroads][:names][:reading_railroad] = {mortgage_value: 100}
+  result[:railroads][:names][:pennsylvania_railroad] = {mortgage_value: 200}
+  result[:railroads][:names][:b_and_o_railroad] = {mortgage_value: 400}
+  result[:railroads][:names][:shortline_railroad] = {mortgage_value: 800}
 
+  result 
 end
